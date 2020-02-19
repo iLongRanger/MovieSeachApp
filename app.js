b6306df9e9d00a7a2ba41bdc94b0d3249a20393b
EC2 Default User <ec2-user@ip-172-31-37-28.us-east-2.compute.internal>
2020-02-19 00:30:56 +0000

    initial commit, no design - logic working

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+const request = require("request");
+
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res){
+   res.render("search"); 
+});
+
+
+app.get("/results", function (req, res){
+    let search = req.query.search;
+    let url = "http://www.omdbapi.com/?s=" + search + "&apikey=b6d37227";
+    request(url, function (error, response, body){
+        if(!error && response.statusCode == 200){
+            let data = JSON.parse(body);
+            res.render("results", {data: data});
+        }
+    });
+}); 
+
+
+//server listener 
+app.listen(process.env.PORT, process.env.IP, function () { 
+    console.log("movie app has started!")
+}); 
+    
